fb4f941e1e5e8c3402da0ef3612dbe12af4da8f2
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-27 14:00:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,16 @@
 class Song
-  attr_accessor :name, :artist
-  
-  @@all = []
+  attr_accessor :artist, :name
 
   def initialize(name)
     @name = name
   end
 
   def artist_name
-    if @artist == nil
-      return nil
+    if artist
+      self.artist.name
     else
-      @artist.name
+      nil
     end
   end
-end
+
+end 
